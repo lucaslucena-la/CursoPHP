6393e839aa3c9e140e5ad08eaf1337e2810fe976
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2024-12-26 16:32:51 -0300

    Primeiro commit

---
+++ echo_print.php
@@ -0,0 +1,20 @@
+<html>
+    <head> 
+        <meta charset = "utf-8"/>
+        <title> Curso PHP </title>
+
+    </head>
+
+    <body>
+
+        <?php
+            echo "Comando echo<br/>";
+            
+            print ("Comando print<hr/>");
+        ?>
+        <br/>
+
+
+    </body>
+
+</html>
