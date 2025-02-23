c527b12f78821b97943037f87e723bc3243b8b18
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-02-23 19:33:29 -0300

    a

---
+++ loops_while.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+        $num = 1;
+
+        while($num < 10){
+            $num ++;
+        }
+
+        echo '-- Inicio do loop --'
+
+    ?>
+</body>
+
+</html>
